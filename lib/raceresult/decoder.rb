c10dc411be352f952b7fdab9d65c5cfb4c3a3f33
Yannick Eckey <yannick.eckey@googlemail.com>
2015-10-02 13:50:39 +0000

    Implement basic status checks

--- lib/raceresult/decoder.rb
+++ lib/raceresult/decoder.rb
@@ -1,10 +1,33 @@
+require 'socket'
+
 module RaceResult
   class Decoder
     attr_accessor :host, :port
 
-    def initialize(host, port: 3601)
+    STATUS_KEYS = [:date, :time, :has_power, :antennas, :timing_mode, :file_no,
+                   :gps_fix, :location, :rfid_ok]
+
+    def initialize(host, port = 3601)
       self.host = host
       self.port = port
+
+      connect
+    end
+
+    def has_power?
+      get_status[:has_power] == "1"
+    end
+
+
+  private
+    def connect
+      @socket = TCPSocket.new(host, port)
+    end
+
+    def get_status
+      @socket.puts 'GETSTATUS'
+      status = @socket.gets.chomp.split(';')
+      Hash[STATUS_KEYS.zip(status[1..-1])]
     end
   end
 end
